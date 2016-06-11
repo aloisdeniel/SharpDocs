f7f0448aae0b2421ee72d1d52ff4fed8640393f0
Aloïs Deniel <alois.deniel@gmail.com>
2016-06-12 00:18:20 +0200

    Added icon, moved html generation code.

---
+++ Sources/SharpDocs/Formatters/HtmlFormatter.cs
@@ -0,0 +1,120 @@
+﻿using HandlebarsDotNet;
+using SharpDocs.Documentation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDocs.Formatters
+{
+    public class HtmlFormatter
+    {
+        private void Generate(TextWriter writer, Parsers.Entities.Content content)
+        {
+            foreach (var item in content)
+            {
+                if (item is Parsers.Entities.Content.Text)
+                {
+                    writer.WriteSafeString($"{(item as Parsers.Entities.Content.Text).Value}");
+                }
+                else if (item is Parsers.Entities.Content.See)
+                {
+                    writer.WriteSafeString($"<b>{(item as Parsers.Entities.Content.See).Reference}</b>");
+                }
+                else if (item is Parsers.Entities.Content.Code)
+                {
+                    writer.WriteSafeString($"<pre><code>{(item as Parsers.Entities.Content.Code).Value}</code></pre>");
+                }
+                else if (item is Parsers.Entities.Content.InlineCode)
+                {
+                    writer.WriteSafeString($"<code>{(item as Parsers.Entities.Content.Code).Value}</code>");
+                }
+                else if (item is Parsers.Entities.Content.List)
+                {
+                    var list = item as Parsers.Entities.Content.List;
+
+                    var listtype = "ul";
+                    if (list.Type == "number")
+                    {
+                        listtype = "ol";
+                    }
+
+                    if (list.Header != null)
+                    {
+                        writer.WriteSafeString($"<h3>{list.Header.Value}</h3>\n");
+                    }
+
+                    if (list.Items != null)
+                    {
+                        writer.WriteSafeString($"<{listtype}>");
+
+                        foreach (var listitem in list.Items)
+                        {
+                            writer.WriteSafeString($"<li>");
+                            Generate(writer, listitem.Value);
+                            writer.WriteSafeString($"</li>");
+                        }
+
+                        writer.WriteSafeString($"</{listtype}>");
+                    }
+                }
+            }
+        }
+        
+        private string LoadTemplate(string name)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var resourceName = $"SharpDocs.Templates.{name}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+  
+        public string Render(Assembly tree, string template = "Default.html")
+        {
+
+            Handlebars.RegisterHelper("msdoc", (writer, context, parameters) => {
+                var member = context as Parsers.Entities.Member;
+
+                if (member != null)
+                {
+                    if (member.TypeParameters.Any())
+                    {
+                        writer.WriteSafeString("<ul class='typeparams'>");
+                        foreach (var p in member.TypeParameters)
+                        {
+                            writer.WriteSafeString($"<li><b>{p.Name}</b>");
+                            Generate(writer, p.Summary);
+                            writer.WriteSafeString("</li>");
+                        }
+                        writer.WriteSafeString("</ul>");
+                    }
+
+                    if (member.Parameters.Any())
+                    {
+                        writer.WriteSafeString("<ul class='args'>");
+                        foreach (var p in member.Parameters)
+                        {
+                            writer.WriteSafeString($"<li><b>{p.Name}</b>");
+                            Generate(writer, p.Summary);
+                            writer.WriteSafeString("</li>");
+                        }
+                        writer.WriteSafeString("</ul>");
+                    }
+
+                    Generate(writer, member.Summary);
+                }
+
+            });
+
+            var render = Handlebars.Compile(LoadTemplate(template));
+            return render(tree);
+        }
+    }
+}
--- Sources/SharpDocs/SharpDocs.cs
+++ Sources/SharpDocs/SharpDocs.cs
@@ -8,6 +8,7 @@ using System.Xml.Linq;
 
 namespace SharpDocs
 {
+    using Formatters;
     using Parsers;
 
     public class SharpDocs
@@ -213,75 +214,7 @@ namespace SharpDocs
 
         #endregion
 
-        private string LoadTemplate(string name)
-        {
-            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            var resourceName = $"SharpDocs.Templates.{name}";
 
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        public string Generate(string dllFile)
-        {
-            var template = this.LoadTemplate("Default.html");
-            return Generate(dllFile, template, ".html");
-        }
-
-        private void Generate(TextWriter writer, Parsers.Entities.Content content)
-        {
-            foreach (var item in content)
-            {
-                if (item is Parsers.Entities.Content.Text)
-                {
-                    writer.WriteSafeString($"{(item as Parsers.Entities.Content.Text).Value}");
-                }
-                else if (item is Parsers.Entities.Content.See)
-                {
-                    writer.WriteSafeString($"<b>{(item as Parsers.Entities.Content.See).Reference}</b>");
-                }
-                else if (item is Parsers.Entities.Content.Code)
-                {
-                    writer.WriteSafeString($"<pre><code>{(item as Parsers.Entities.Content.Code).Value}</code></pre>");
-                }
-                else if (item is Parsers.Entities.Content.InlineCode)
-                {
-                    writer.WriteSafeString($"<code>{(item as Parsers.Entities.Content.Code).Value}</code>");
-                }
-                else if (item is Parsers.Entities.Content.List)
-                {
-                    var list = item as Parsers.Entities.Content.List;
-
-                    var listtype = "ul";
-                    if (list.Type == "number")
-                    {
-                        listtype = "ol";
-                    }
-
-                    if (list.Header != null)
-                    {
-                        writer.WriteSafeString($"<h3>{list.Header.Value}</h3>\n");
-                    }
-
-                    if (list.Items != null)
-                    {
-                        writer.WriteSafeString($"<{listtype}>");
-
-                        foreach (var listitem in list.Items)
-                        {
-                            writer.WriteSafeString($"<li>");
-                            Generate(writer, listitem.Value);
-                            writer.WriteSafeString($"</li>");
-                        }
-
-                        writer.WriteSafeString($"</{listtype}>");
-                    }
-                }
-            }
-        }
 
         public string Generate(string dllFile, string template, string ext)
         {
@@ -292,46 +225,11 @@ namespace SharpDocs
             var doc = new MsDocParser().Parse(xmlDocFile);
 
             var tree = CreateTree(doc,assembly);
-            
+
+            var content = new HtmlFormatter().Render(tree);
+
             var outputFile = dllFile.Replace(".dll", ext);
-
-            Handlebars.RegisterHelper("msdoc", (writer, context, parameters) => {
-                var member = context as Parsers.Entities.Member;
-
-                if(member != null)
-                {
-                    if (member.TypeParameters.Any())
-                    {
-                        writer.WriteSafeString("<ul class='typeparams'>");
-                        foreach (var p in member.TypeParameters)
-                        {
-                            writer.WriteSafeString($"<li><b>{p.Name}</b>");
-                            Generate(writer, p.Summary);
-                            writer.WriteSafeString("</li>");
-                        }
-                        writer.WriteSafeString("</ul>");
-                    }
-
-                    if (member.Parameters.Any())
-                    {
-                        writer.WriteSafeString("<ul class='args'>");
-                        foreach (var p in member.Parameters)
-                        {
-                            writer.WriteSafeString($"<li><b>{p.Name}</b>");
-                            Generate(writer, p.Summary);
-                            writer.WriteSafeString("</li>");
-                        }
-                        writer.WriteSafeString("</ul>");
-                    }
-
-                    Generate(writer, member.Summary);
-                }
-                
-            });
-
-            var render = Handlebars.Compile(template);
-            var content = render(tree);
-
+            
             File.WriteAllText(outputFile, content);
 
             return content;
